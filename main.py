f71712dbf4ce2e3f682f8940de2c67369ba6cc2d
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2022-08-04 15:17:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
-from flask import Flask,request,json,jsonify
 import os
 import PIL
-from PIL import Image, ImageDraw, ImageFont
-app = Flask(__name__)
 import logging
+import requests 
+from flask import Flask,request,json,send_file
 from logging import warning,error,info,debug
-import requests
-from flask import send_file
-@app.route('/',methods=['get'])
-def hello():
-    return {'working':True}
+from PIL import Image, ImageDraw, ImageFont
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+  return {'working':True}
 
 @app.route('/hi/<text>',methods=['GET'])
-def he():
-    txt = "Hello"
-    img = Image.open("nordwood-themes-R53t-Tg6J4c-unsplash.jpg")
-    d1 = ImageDraw.Draw(img)
-    myFont = ImageFont.truetype("ds.otf", 50)
-    d1.text((65, 10), txt, fill =(0, 0, 0),font=myFont)
-    img.save("result.jpg")
-    filename = "result.jpg"
-    return send_file(filename, mimetype='image/jpeg')
+def hello():
+  txt = "Hello"
+  img = Image.open("nordwood-themes-R53t-Tg6J4c-unsplash.jpg")
+  d1 = ImageDraw.Draw(img)
+  myFont = ImageFont.truetype("ds.otf", 50)
+  d1.text((65, 10), txt, fill =(0, 0, 0),font=myFont)
+  img.save("result.jpg")
+  filename = "result.jpg"
+  return send_file(filename, mimetype='image/jpeg')
+
 @app.route('/write', methods=['GET'])
 def write():
-    txt = request.args.get('text', None)
-    img = Image.open("nordwood-themes-R53t-Tg6J4c-unsplash.jpg")
-    d1 = ImageDraw.Draw(img)
-    myFont = ImageFont.truetype("ds.otf", 50)
-    d1.text((65, 10), txt, fill =(0, 0, 0),font=myFont)
-    img.save("result.jpg")
-    filename = "result.jpg"
-    return send_file(filename, mimetype='image/jpeg')
+   txt = request.args.get('text', None)
+   img = Image.open("nordwood-themes-R53t-Tg6J4c-unsplash.jpg")
+   d1 = ImageDraw.Draw(img)
+   myFont = ImageFont.truetype("ds.otf", 50)
+   d1.text((65, 10), txt, fill =(0, 0, 0),font=myFont)
+   img.save("result.jpg")
+   filename = "result.jpg"
+   return send_file(filename, mimetype='image/jpeg')
+    
+    
 if __name__ == '__main__':
     app.run(host = '0.0.0.0' , port = os.environ.get('PORT', 5000),debug=True)
