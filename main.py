996c082e53bb57318defdffcd75c17ad5c277bb0
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2022-08-04 17:36:58 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 import os
 import PIL
 import logging
+import textwrap 
 import requests 
+from urllib.parse import quote as q
 from flask import Flask,request,json,send_file
 from logging import warning,error,info,debug
 from PIL import Image, ImageDraw, ImageFont
 
-
 app = Flask(__name__)
+
+def wrap(value):
+  wrapper = textwrap.TextWrapper(width=65)
+  word_list = wrapper.wrap(text=value)
+  s = []
+  for element in word_list:
+      s.append(element)
+  hmm = "\n"
+  x = q(hmm.join(s))
+  return x
 
 @app.route('/')
 def test():
-  return {'working':True}
-
-@app.route('/hi/<text>',methods=['GET'])
-def hello():
-  txt = "Hello"
-  img = Image.open("nordwood-themes-R53t-Tg6J4c-unsplash.jpg")
-  d1 = ImageDraw.Draw(img)
-  myFont = ImageFont.truetype("ds.otf", 120)
-  d1.text((65, 10), txt, fill =(0, 0, 0),font=myFont)
-  img.save("result.jpg")
-  filename = "result.jpg"
-  return send_file(filename, mimetype='image/jpeg')
+  return {
+    'Owner': Naveen,
+    'Telegram': Naveen_xD,
+    'GitHub': Naveen-xd2580
+  }
 
 @app.route('/write', methods=['GET'])
 def write():
-   txt = request.args.get('text', None)
+   txxt = request.args.get('text', None)
+   txt = wrap(txxt)
    img = Image.open("nordwood-themes-R53t-Tg6J4c-unsplash.jpg")
    d1 = ImageDraw.Draw(img)
    myFont = ImageFont.truetype("ds.otf", 120)
