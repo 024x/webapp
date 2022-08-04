6b7a6a02e1b8b5b10d19ec68e93755931a0c2346
sattttya <satyendrabongi@gmail.com>
2022-08-04 12:50:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,19 @@ from flask import send_file
 def hello():
     return {'working':True}
 
-@app.route('/hi', methods=['POST'])
+@app.route('/hi/<text>',methods=['GET'])
 def he():
     txt = "Hello"
+    img = Image.open("nordwood-themes-R53t-Tg6J4c-unsplash.jpg")
+    d1 = ImageDraw.Draw(img)
+    myFont = ImageFont.truetype("ds.otf", 50)
+    d1.text((65, 10), txt, fill =(0, 0, 0),font=myFont)
+    img.save("result.jpg")
+    filename = "result.jpg"
+    return send_file(filename, mimetype='image/jpeg')
+@app.route('/write', methods=['GET'])
+def write():
+    text = request.args.get('text', None)
     img = Image.open("nordwood-themes-R53t-Tg6J4c-unsplash.jpg")
     d1 = ImageDraw.Draw(img)
     myFont = ImageFont.truetype("ds.otf", 50)
