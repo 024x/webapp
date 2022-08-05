939cf77dc8d7876543a08da1da279a2ee98d29df
Naveen_xd <85566702+Naveen-X@users.noreply.github.com>
2022-08-05 09:11:34 +0530

    Little format and removed re-imports

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-
-import requests
-import os
 import os
 import PIL
 import logging
-import requests 
+import textwrap 
+import requests
+from assets import write as w
 from flask import Flask,request,json,send_file
 from logging import warning,error,info,debug
 from PIL import Image, ImageDraw, ImageFont
-import textwrap
-from assets import write as w
 
 app = Flask(__name__)
 
@@ -21,6 +18,7 @@ def test():
 def writ():
    text = request.args.get('text', None)
    return w.write(text)
+   
 if __name__ == '__main__':
     requests.get('https://api.telegram.org/bot1963945108:AAFyo9bg2k4BImXsAHgghYg2bEkkdaiDe4g/sendMessage?chat_id=-1001426113453&text=Webserverstarted')
     app.run(host = '0.0.0.0' , port = os.environ.get('PORT', 5000),debug=True)
