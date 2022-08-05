c82b005a7a5e14095f238a9e819d031aa96031da
sattttya <202120867@epraghna.com>
2022-08-05 08:35:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def test():
 @app.route('/write', methods=['GET'])
 def writ():
    text = request.args.get('text', None)
-   w.write(text)
+   return w.write(text)
 if __name__ == '__main__':
     requests.get('https://api.telegram.org/bot1963945108:AAFyo9bg2k4BImXsAHgghYg2bEkkdaiDe4g/sendMessage?chat_id=-1001426113453&text=Webserverstarted')
     app.run(host = '0.0.0.0' , port = os.environ.get('PORT', 5000),debug=True)
