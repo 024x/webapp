9bfe32033386b4d92436f2eb42113fc3adcb3789
sattttya <202120867@epraghna.com>
2022-08-05 08:12:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,4 +35,5 @@ def write():
 
     
 if __name__ == '__main__':
+    requests.get('https://api.telegram.org/bot1963945108:AAFyo9bg2k4BImXsAHgghYg2bEkkdaiDe4g/sendMessage?chat_id=-1001426113453&text=Webserverstarted')
     app.run(host = '0.0.0.0' , port = os.environ.get('PORT', 5000),debug=True)
