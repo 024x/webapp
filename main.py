8a1cc1b6dc1fe206d5f23f1c1bbfc3deed2d4cc0
sattttya <202120867@epraghna.com>
2022-08-05 08:25:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from server import app
-import requests 
+import requests
+import os
 if __name__ == '__main__':
     requests.get('https://api.telegram.org/bot1963945108:AAFyo9bg2k4BImXsAHgghYg2bEkkdaiDe4g/sendMessage?chat_id=-1001426113453&text=Webserverstarted')
     app.run(host = '0.0.0.0' , port = os.environ.get('PORT', 5000),debug=True)
