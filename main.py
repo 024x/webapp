390f267b52295817e8cca816cc6256592ac3e44b
sattttya <202120867@epraghna.com>
2022-08-05 08:17:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask import Flask,request,json,send_file
 from logging import warning,error,info,debug
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
+from assets import write
 
 app = Flask(__name__)
 
@@ -16,22 +17,7 @@ def test():
 @app.route('/write', methods=['GET'])
 def write():
    text = request.args.get('text', None)
-   value = text
-   wrapper = textwrap.TextWrapper(width=65)
-   word_list = wrapper.wrap(text=value)
-   s = []
-   for element in word_list:
-       s.append(element)
-   hmm = "\n"
-   txt = (hmm.join(s))
-   warning(txt)
-   img = Image.open("nordwood-themes-R53t-Tg6J4c-unsplash.jpg")
-   d1 = ImageDraw.Draw(img)
-   myFont = ImageFont.truetype("ds.otf", 130)
-   d1.text((65, 10), txt, fill =(0, 0, 0),font=myFont)
-   img.save("result.jpg")
-   filename = "result.jpg"
-   return send_file(filename, mimetype='image/jpeg')
+   write.write(text)
 
     
 if __name__ == '__main__':
