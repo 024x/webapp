e9062fbaf132eff162b8ee04cd9259aa76ea0dd1
sattttya <202120867@epraghna.com>
2022-08-05 08:16:18 +0530

    Create write.py

---
+++ assets/write.py
@@ -0,0 +1,26 @@
+import os
+import PIL
+import logging
+import requests 
+from flask import Flask,request,json,send_file
+from logging import warning,error,info,debug
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+
+def write(text):
+   value = text
+   wrapper = textwrap.TextWrapper(width=65)
+   word_list = wrapper.wrap(text=value)
+   s = []
+   for element in word_list:
+       s.append(element)
+   hmm = "\n"
+   txt = (hmm.join(s))
+   warning(txt)
+   img = Image.open("nordwood-themes-R53t-Tg6J4c-unsplash.jpg")
+   d1 = ImageDraw.Draw(img)
+   myFont = ImageFont.truetype("ds.otf", 130)
+   d1.text((65, 10), txt, fill =(0, 0, 0),font=myFont)
+   img.save("result.jpg")
+   filename = "result.jpg"
+   return send_file(filename, mimetype='image/jpeg')
